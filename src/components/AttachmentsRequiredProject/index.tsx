89ed86ac4f37add027e7fda003cd27f10ec4d424
lennonsousa <lennonsousa@outlook.com>
2021-08-24 12:24:24 -0300

    Types checked.

--- src/components/AttachmentsRequiredProject/index.tsx
+++ src/components/AttachmentsRequiredProject/index.tsx
@@ -53,7 +53,7 @@ const AttachmentsRequiredProject: React.FC<AttachmentRequiredProps> = ({ attachm
             await handleListAttachmentsRequired();
         }
         catch (err) {
-            console.log("Error to pause attachmentRequired");
+            console.log("Error to pause required attachment");
             console.log(err);
         }
 
